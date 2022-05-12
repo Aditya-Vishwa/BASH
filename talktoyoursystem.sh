0f8a8048a1a9e02f0f79e4d35dee9f958f708f76
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2022-05-12 19:39:45 +0000

    It's Super Cool try now.

---
+++ talktoyoursystem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Hey! Buddy"
+
+sleep 2
+
+echo "How are you?"
+
+sleep 3
+
+echo "I am fine too"
+
+sleep 1
+
+echo "You're logged in as $whoami in the directory $pwd and currently, It's $date"
+
+sleep 3 
+
+echo "Okay! Go Ahead. BYE! BYE!"
