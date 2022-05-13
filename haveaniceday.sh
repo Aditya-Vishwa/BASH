fcadce4be81df720656be9286024e0b00e28cb4e
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2022-05-13 14:52:22 +0000

    Have a Nice Day
    Execute By:
    ./haveaniceday.sh Name Complement

---
+++ haveaniceday.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name=$1
+compliment=$2
+echo "Hey! $name, Your are a very nice person."
+sleep 2
+echo "I learned how to be a good person by watching how you treat others."
+sleep 2
+echo "I think your have the most beautiful $compliment."
+sleep 2
+echo "Have a nice day, $name."
+echo "Bye! Bye!"
